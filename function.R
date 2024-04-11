3d173e902f28d93c13a636976b0a0e806857bc2b
Francisco Segovia <165854179+SirCornflake@users.noreply.github.com>
2024-04-11 15:45:57 -0600

    Add files via upload

--- function.R
+++ function.R
@@ -460,146 +460,150 @@ gibbs_NBR<-function(y, Covariates, first_excluded=0, nchain=1000, burnin=0, tau2
 
 
 ## Gibbs for Quantile regression
-gibbs_QR<-function(y, Covariates, first_excluded=0, nchain=1000, burnin=0, alpha=0.5, tau2=1000, rho=1, a0=1, b0=1,
-                   beta.ini=base::rep(1,length(Covariates[1,]) +1), invsigma2.ini=1, w.ini=base::rep(1,length(y)), count.iteration=TRUE )
+gibbs_QR<-function(y, covariables, first_excluded=0, nchain=1000, burnin=0, alpha=0.5, tau2=1000, rho=1, a0=1, b0=1,
+beta.ini=rep(1,length(covariables[1,]) +1), invsigma2.ini=1, w.ini=rep(1,length(y)), count.iteration=TRUE )
 {
-  t0<-proc.time()
-  ##############################
-  ##	   Initiating	    ##
-  ##############################
-  N <- length(y)
-  p<-length(Covariates[1,]) +1
-  if(length(colnames(Covariates))==0)
-  {
-    aux_prednames<-base::vector(length=ncol(Covariates)); for(i in 1:ncol(Covariates)){aux_prednames[i]<-c(paste0("X",i))}
-    colnames(Covariates)<-aux_prednames
-  }
-  X<-cbind(base::rep(1,N), Covariates)
-  X<-as.matrix(X)
-  colnames(X)<-NULL
-  intercept_first_excluded<- first_excluded +1
-  p_selection<-p -intercept_first_excluded
+ t0<-proc.time()
+	##############################
+	##	   Initiating	    ##
+	##############################
+	N <- length(y)
+	p<-length(covariables[1,]) +1
+	if(length(colnames(covariables))==0)
+	{
+		aux_prednames<-vector(length=ncol(covariables)); for(i in 1:ncol(covariables)){aux_prednames[i]<-c(paste0("X",i))}
+		colnames(covariables)<-aux_prednames
+	}
+	X<-cbind(rep(1,N), covariables)
+	X<-as.matrix(X)
+	colnames(X)<-NULL
+	intercept_first_excluded<- first_excluded +1	
+	p_selection<-p -intercept_first_excluded				
 
-  beta.chain<-matrix(beta.ini, nrow=nchain, ncol=p, byrow=TRUE)
-  beta<-beta.chain[1,]
-  invsigma2.chain<-matrix(c(invsigma2.ini), nrow=nchain, ncol=1, byrow=TRUE)
-  invsigma2<-invsigma2.chain[1,]
-  sigma2.chain<-matrix(0, nrow=nchain, ncol=1, byrow=TRUE)
-  w<-w.ini
+	beta.cadenas<-matrix(beta.ini, nrow=nchain, ncol=p, byrow=TRUE)
+	beta<-beta.cadenas[1,]
+	invsigma2.cadenas<-matrix(c(invsigma2.ini), nrow=nchain, ncol=1, byrow=TRUE)
+	invsigma2<-invsigma2.cadenas[1,]
+	sigma2.cadenas<-matrix(0, nrow=nchain, ncol=1, byrow=TRUE)
+	w<-w.ini
 
-  gammaOld<-base::rep(1, p_selection)
+	gammaOld<-rep(1, p_selection)	
+	
+	delta2<-2/(alpha*(1-alpha))
+	xi<-(1 -2*alpha)/(alpha*(1-alpha))
+	Z<-matrix(y -xi*w)
 
-  delta2<-2/(alpha*(1-alpha))
-  xi<-(1 -2*alpha)/(alpha*(1-alpha))
-  Z<-matrix(y -xi*w)
+	model_cadenas<-matrix(NA, nrow=(nchain- burnin), ncol=p_selection)
 
-  model_chain<-matrix(NA, nrow=(nchain- burnin), ncol=p_selection)
+	log_tau2<-log(tau2)
+	frac_tau2<-1/tau2
+	womack_prior<-womack(p_selection,rho)				
+	InvDiag_tau2_general<-diag(frac_tau2, p)			
+	Omega_modified_general<-matrix(rep(1/(delta2*w)*invsigma2, each = p), nrow = p, ncol = N, byrow = F) 	
+	tXgamma_Omega_general<-t(X)*Omega_modified_general[1:ncol(X),]
 
-  log_tau2<-base::log(tau2)
-  frac_tau2<-1/tau2
-  log_womack_prior<-base::log(womack(p_selection,rho))
-  InvDiag_tau2_general<-diag(frac_tau2, p)
-  Omega_modified_general<-matrix(base::rep(1/(delta2*w)*invsigma2, each = p), nrow = p, ncol = N, byrow = F)
-  tXgamma_Omega_general<-t(X)*Omega_modified_general[1:ncol(X),]
-  NotInverse_V_general<-tXgamma_Omega_general%*%X + InvDiag_tau2_general
-  tXgamma_Omega_Z_general<- tXgamma_Omega_general%*%Z
+	######################
+	## 	 Gibbs	  ##
+	######################
+	for(i in 1:nchain)
+	{
+		if(count.iteration==TRUE){cat("  Iteracion", i, "de", nchain, "\r")}
+			
+			ifelse( sum(gammaOld) == 0 | sum(gammaOld) ==p_selection,		
+			AlgorithmChosen<-1,
+			AlgorithmChosen<-sample(c(1,2), size=1, prob=c(0.5, 0.5))) 
+	
+			if(AlgorithmChosen==1)		#Add-delete
+			{ 
+				aux_gamma<-sample(1:p_selection, size=1)
+				gammaCandidate<-gammaOld
+				gammaCandidate[aux_gamma]<-1 -gammaOld[aux_gamma]
+			}
+			if(AlgorithmChosen==2)		#Swapping
+			{ 
+				gammaOld_0<-which(gammaOld==0)
+				ifelse(length(gammaOld_0)==1, gammaOld_0Selected<-gammaOld_0, gammaOld_0Selected<-sample(which(gammaOld==0), size=1))
+				gammaOld_1<-which(gammaOld==1)
+				ifelse(length(gammaOld_1)==1, gammaOld_1Selected<-gammaOld_1, gammaOld_1Selected<-sample(which(gammaOld==1), size=1))
+				gammaCandidate<-gammaOld
+				gammaCandidate[c(gammaOld_0Selected, gammaOld_1Selected)]<-c(1,1) -gammaOld[c(gammaOld_0Selected, gammaOld_1Selected)]	
+			}
 
-  X_index_aux<-base::vector("list", length = 2)
-  X_gamma_aux<-base::vector("list", length = 2)
-  tXgamma_Omega_aux<-base::vector("list", length = 2)
-  chol_V_aux<-base::vector("list", length = 2)
-  V_aux<-base::vector("list", length = 2)
-  m_aux<-base::vector("list", length = 2)
-  X_index_aux_excluded<-c(1:intercept_first_excluded)
-  DAlog_weight_model<-base::vector(length=2)
+			DAlog_weight_model<-vector(length=2)
+			for(k in 1:2)
+			{	
+				if(k==1){gamma_aux<-gammaOld}else{gamma_aux<-gammaCandidate}
+				q_aux<-sum(gamma_aux)	
+				X_index_aux<-unique(c(1:intercept_first_excluded, intercept_first_excluded +which(gamma_aux==1)))
+				X_gamma_aux<-X[,X_index_aux] 
+				tXgamma_Omega_aux<- tXgamma_Omega_general[X_index_aux,]
+				InvDiag_tau2_aux<-InvDiag_tau2_general[1:(intercept_first_excluded +q_aux), 1:(intercept_first_excluded +q_aux)]
+				V_aux<- tXgamma_Omega_aux%*%X_gamma_aux + InvDiag_tau2_aux		
+				m_aux<-solve(V_aux,tXgamma_Omega_aux%*%Z)					
+				DAlog_weight_model[k]<-as.vector( (-q_aux/2)*log_tau2 +(0.5)*log(det(solve(V_aux))) +
+				(0.5)*t(m_aux)%*%V_aux%*%m_aux +log(womack_prior[q_aux +1]) )
+			}
+			A<- DAlog_weight_model -max(DAlog_weight_model)
+			b<-exp(A)
+			DAgamma_prob<-b/sum(b)
 
-
-  ######################
-  ## 	 Gibbs	  ##
-  ######################
-  for(i in 1:nchain)
-  {
-    if(count.iteration==TRUE){cat("  Iteracion", i, "de", nchain, "\r")}
-
-    ## Add-delete algorithm
-    aux_gamma<-sample(1:p_selection, size=1)
-    gammaCandidate<-gammaOld
-    gammaCandidate[aux_gamma]<-1 -gammaOld[aux_gamma]
-
-
-
-    for(k in 1:2)
-    {
-      if(k==1){gamma_aux<-gammaOld}else{gamma_aux<-gammaCandidate}
-      q_aux<-base::sum(gamma_aux)
-      X_index_aux[[k]]<-unique(c(X_index_aux_excluded, intercept_first_excluded +which(gamma_aux==1)))
-      X_gamma_aux[[k]]<-X[,X_index_aux[[k]]]
-      tXgamma_Omega_aux[[k]]<- tXgamma_Omega_general[X_index_aux[[k]],]
-      tXgamma_Omega_aux_Z<- tXgamma_Omega_Z_general[X_index_aux[[k]]]
-      InvDiag_tau2_aux<-InvDiag_tau2_general[1:(intercept_first_excluded +q_aux), 1:(intercept_first_excluded +q_aux)]
-      chol_V_aux[[k]]<- chol(NotInverse_V_general[X_index_aux[[k]], X_index_aux[[k]]])
-      V_aux[[k]]<- chol2inv(chol_V_aux[[k]])
-      det_V_aux<-1/prod(diag(chol_V_aux[[k]]))^(2)
-      if(det_V_aux<1e-10){det_V_aux<-1e-10}
-      m_aux[[k]]<-V_aux[[k]]%*%tXgamma_Omega_aux_Z
-      DAlog_weight_model[k]<-as.vector( (-q_aux/2)*log_tau2 +(0.5)*base::log(det_V_aux) +
-                                          (0.5)*t(m_aux[[k]])%*%tXgamma_Omega_aux_Z +log_womack_prior[q_aux +1] )
-    }
-    A<- DAlog_weight_model -base::max(DAlog_weight_model)
-    b<-exp(A)
-    DAgamma_prob<-b/base::sum(b)
-
-    p_gammaOld<-DAgamma_prob[1]
-    p_gammaCandidate<-DAgamma_prob[2]
-    p_selectionCandidate<-min(p_gammaCandidate/p_gammaOld,1)
-    selection<-sample(c("Candidate","Old"), size=1, prob=c(p_selectionCandidate, 1-p_selectionCandidate))
-    if(selection=="Candidate"){gamma<-gammaCandidate; k<-2}else{gamma<-gammaOld; k<-1}
-    q<-base::sum(gamma)
+			p_gammaOld<-DAgamma_prob[1]
+			p_gammaCandidate<-DAgamma_prob[2]
+			p_selectionCandidate<-min(p_gammaCandidate/p_gammaOld,1)
+			selection<-sample(c("Candidate","Old"), size=1, prob=c(p_selectionCandidate, 1-p_selectionCandidate))
+			ifelse(selection=="Candidate", {gamma<-gammaCandidate;}, {gamma<-gammaOld;})
+			q<-sum(gamma)
 
 
-    ## Updating beta
-    beta_index_0<-setdiff(1:p, X_index_aux[[k]])
-    if(length(beta_index_0)==0){beta_index_0<-0}
-    beta[X_index_aux[[k]]]<-m_aux[[k]] +chol(V_aux[[k]])%*%as.matrix(rnorm(length(X_index_aux[[k]])))
-    beta[beta_index_0]<-base::rep(0,length(beta_index_0))
-    beta.chain[i,] <- beta
-    if(i>burnin){model_chain[i -burnin,]<-gamma}
+		## Updating beta
+		which_gamma_1<-which(gamma==1)
+		X_index<-c(1:intercept_first_excluded, intercept_first_excluded +which_gamma_1)
+		X_gamma<-X[,X_index]
+		tXgamma_Omega<- tXgamma_Omega_general[X_index,]
+		InvDiagtau2<-InvDiag_tau2_general[1:(intercept_first_excluded +q), 1:(intercept_first_excluded +q)]
+		beta_index_0<-intercept_first_excluded +which(gamma==0)	
+		beta_index_1<- intercept_first_excluded +which(gamma==1)	
+		V_gamma <- tXgamma_Omega%*%X_gamma + InvDiagtau2		
+		m_gamma <- solve(V_gamma,tXgamma_Omega%*%Z)		
+		aux_beta<- t(rmvnorm(n = 1, mean = m_gamma, sigma = solve(V_gamma)))	
+		beta[c(1:intercept_first_excluded, beta_index_1)]<-aux_beta
+		beta[beta_index_0]<-rep(0,length(beta_index_0))	
+		beta.cadenas[i,] <- beta											
+		if(i>burnin){model_cadenas[i -burnin,]<-gamma}						
+	
+		## Updating w
+		beta_index_1<- intercept_first_excluded +which(gamma==1)	
+		X_beta <- as.matrix(X_gamma)%*%beta[c(1:intercept_first_excluded, beta_index_1)]
+		chi<-invsigma2*((y -X_beta)^2)/delta2; psi<- invsigma2*(xi^2 +2*delta2)/delta2
+		for(j in 1:length(y))
+		{
+			w[j]<-rgig(1, lambda=1/2, chi=chi[j], psi=psi)
+		}
 
-    ## Updating w
-    X_beta <- as.matrix(X_gamma_aux[[k]])%*%as.matrix(beta[X_index_aux[[k]]])
-    chi<-invsigma2*((y -X_beta)^2)/delta2; psi<- invsigma2*(xi^2 +2*delta2)/delta2
-    for(j in 1:length(y))
-    {
-      w[j]<-GIGrvg::rgig(1, lambda=1/2, chi=chi[j], psi=psi)
-    }
+		## Updating invsigma2=1/sigma2
+		a1<-a0 +3*N/2
+		b1<-b0 +sum( ((y -X_beta -xi*w)^2 +2*delta2*w^2)/(2*delta2*w) )
+		invsigma2<-rgamma( 1, shape= a1, rate= b1 )
+		invsigma2.cadenas[i,]<-invsigma2
+		
+		## Updating  gamma
+		gammaOld<-gamma
 
-    ## Updating invsigma2=1/sigma2
-    a1<-a0 +3*N/2
-    b1<-b0 +base::sum( ((y -X_beta -xi*w)^2 +2*delta2*w^2)/(2*delta2*w) )
-    invsigma2<-rgamma( 1, shape= a1, rate= b1 )
-    invsigma2.chain[i,]<-invsigma2
+		Z<-matrix(y -xi*w)
+		Omega_modified_general<-matrix(rep(1/(delta2*w)*invsigma2, each = p), nrow = p, ncol = N, byrow = F)
+		tXgamma_Omega_general<-t(X)*Omega_modified_general[1:ncol(X),]
+	}
+	beta.cadenas<-beta.cadenas[(burnin +1):nchain,]
+	sigma2.cadenas<-1/invsigma2.cadenas[(burnin +1):nchain,]
 
-    ## Updating  gamma
-    gammaOld<-gamma
+	if(first_excluded!=0)
+	{
+		prednames<-colnames(covariables)[-(1:first_excluded)]
+	}else{prednames<-colnames(covariables)}
+	t1<-proc.time()
+	Time<-(t1-t0)[3]
 
-    ## Updating other quantities
-    Z<-matrix(y -xi*w)
-    Omega_modified_general<-matrix(base::rep(1/(delta2*w)*invsigma2, each = p), nrow = p, ncol = N, byrow = F)
-    tXgamma_Omega_general<-t(X)*Omega_modified_general[1:ncol(X),]
-    NotInverse_V_general<-tXgamma_Omega_general%*%X + InvDiag_tau2_general
-    tXgamma_Omega_Z_general<- tXgamma_Omega_general%*%Z
-  }
-  beta.chain<-beta.chain[(burnin +1):nchain,]
-  sigma2.chain<-1/invsigma2.chain[(burnin +1):nchain,]
-
-  if(first_excluded!=0)
-  {
-    prednames<-colnames(Covariates)[-(1:first_excluded)]
-  }else{prednames<-colnames(Covariates)}
-  t1<-proc.time()
-  Time<-(t1-t0)[3]
-
-  list(beta_chain=beta.chain, sigma2_chain=sigma2.chain, model_chain=model_chain, Regression="QR", prednames=prednames, Seconds=Time)
+	list(beta_cadenas=beta.cadenas, sigma2_cadenas=sigma2.cadenas, model_cadenas=model_cadenas, Regression="QR", prednames=prednames, Seconds=Time)
 }
 
 
@@ -819,7 +823,7 @@ summary_gibbs<-function(fit)
   Quantile<-t(apply(parameters, 2, quantile, prob=c(0.025,0.975)))
   Table<-as.data.frame(matrix(c(Mean,Quantile), byrow=FALSE, ncol=3))
   colnames(Table)<-c("Mean", "2.5% quantile", "97.5% quantile")
-  rowbase::names(Table)<-c(aux_rownames, "intercept", fit$prednames)
+  rownames(Table)<-c(aux_rownames, "intercept", fit$prednames)
 
   ExploredModels<-aux_summary_gibbs(fit)
 
